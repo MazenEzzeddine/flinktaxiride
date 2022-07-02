44b6ad97ddc6f45dcc088a5bb17486bb9eec6bdc
m.ezzeddine <mazen.ezzeddine@gmail.com>
2022-07-02 11:35:38 +0200

    serve events according to their timestamps

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -4,6 +4,7 @@ import org.joda.time.format.DateTimeFormatter;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Locale;
 import java.util.zip.GZIPInputStream;
 
@@ -13,6 +14,8 @@ public class Reader {
     private transient BufferedReader reader;
     private transient InputStream gzipStream;
 
+    public int servingSpeed = 1;
+
 
     private static transient DateTimeFormatter timeFormatter =
             DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.US).withZoneUTC();
@@ -20,48 +23,66 @@ public class Reader {
 
 
 
-     void read() throws IOException {
+     void read() throws IOException, InterruptedException {
          int counter = 0;
+         long servingStartTime = Calendar.getInstance().getTimeInMillis();
+         long dataStartTime=0;
+
          DateTime comp =  DateTime.parse( "2013-01-02 00:00:00", timeFormatter);
         gzipStream = new GZIPInputStream(new FileInputStream("C:\\Users\\m.ezzeddine\\Desktop\\Experiments\\gzippp\\nycTaxiRides.gz"));
         reader = new BufferedReader(new InputStreamReader(gzipStream, "UTF-8"));
          String line;
          TaxiRide ride;
 
-         if (reader.ready() && (line = reader.readLine()) != null) {
+         if ( (line = reader.readLine()) != null) {
              // read first ride
              ride = TaxiRide.fromString(line);
              //System.out.println(ride);
+
+             dataStartTime = getEventTime(ride);
+
              counter++;
              rides.add(ride);
          }
 
+         while ((line = reader.readLine()) != null) {
 
-         if (reader.ready() && (line = reader.readLine()) != null) {
-             // read first ride
-             ride = TaxiRide.fromString(line);
-             counter++;
-             rides.add(ride);
-         }
-
-         while (reader.ready() && (line = reader.readLine()) != null) {
+             long rideEventTime;
              ride = TaxiRide.fromString(line);
              //System.out.println(ride);
-             counter++;
              rides.add(ride);
+             counter++;
+
+             rideEventTime = getEventTime(ride);
+
+             long now = Calendar.getInstance().getTimeInMillis();
+             long servingTime = toServingTime(servingStartTime, dataStartTime, rideEventTime);
+             long waitTime = servingTime - now;
+
+
+             //1 hour = 24344
+
+
+             if(waitTime> 0){
+                 System.out.println("sleeping for " + waitTime);
+             }
+
+             Thread.sleep( (waitTime > 0) ? waitTime : 0);
+
+             System.out.println("serving ride " +  ride.toString());
+
+             if(counter==24344/*1000*/)
+                 break;
+
          }
 
-         System.out.println(counter);
-         int cc =0;
+      /*   System.out.println(counter);
 
          for (TaxiRide r  : rides) {
-             if(r.startTime.isBefore(comp.toInstant())   && r.isStart)
+            // if(r.startTime.isBefore(comp.toInstant())   && r.isStart)
              System.out.println(r);
-             cc++;
-             if(cc == 500)
-                 break;
          }
-
+*/
 
          this.reader.close();
          this.reader = null;
@@ -72,6 +93,21 @@ public class Reader {
 
 
 
+    }
+
+
+    public long toServingTime(long servingStartTime, long dataStartTime, long eventTime) {
+        long dataDiff = eventTime - dataStartTime;
+        return servingStartTime + (dataDiff / this.servingSpeed);
+    }
+
+    public long getEventTime(TaxiRide ride) {
+        if (ride.isStart) {
+            return ride.startTime.getMillis();
+        }
+        else {
+            return ride.endTime.getMillis();
+        }
     }
 
 
